c4ff8324c9ec11a4e024427ceba9fbea230af029
AnnaFiorini <annafiorini@outlook.it>
2018-10-01 00:55:52 +0200

    testUltimo

--- prog_A_v17Client.py
+++ prog_A_v17Client.py
@@ -1731,7 +1731,7 @@ if __name__ == '__main__': #multiprocessing imports script as module
     sockets = []
     hosts = []
     hosts.append("172.31.86.93")
-    hosts.append("172.31.86.97")
+    hosts.append("172.31.95.189")
 #    hosts.append("172.31.95.189")
 #    hosts.append("172.31.86.93")
 #    hosts.append("172.31.86.97")
